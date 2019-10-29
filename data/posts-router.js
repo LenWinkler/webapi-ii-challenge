0b5f4919907850faf2465251473dbd150aed3ab3
LenWinkler <lenwinkler86@gmail.com>
2019-10-29 16:09:06 -0400

    added POST endpoint to add new post

--- data/posts-router.js
+++ data/posts-router.js
@@ -2,6 +2,24 @@ const router = require('express').Router();
 
 const Data = require('./db');
 
+router.post('/', (req, res) => {
+    const newPost = req.body;
+    
+    if(!newPost.title || !newPost.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+
+    } else {
+
+    Data.insert(newPost)
+        .then(reply => {
+            res.status(201).json(newPost)
+        })
+        .catch(err => {
+            console.log('new post error', err);
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+}})
+
 router.get('/', (req, res) => {
     Data.find()
         .then(posts => {
