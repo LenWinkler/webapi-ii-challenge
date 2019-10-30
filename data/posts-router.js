1d9ef1c89dc98995a8e9c7857d0d529a8196609a
LenWinkler <lenwinkler86@gmail.com>
2019-10-29 21:53:00 -0400

    added GET endpoint to get post by id

--- data/posts-router.js
+++ data/posts-router.js
@@ -53,6 +53,23 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Data.findById(id)
+        .then(response => {
+            if(response.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(response)
+            }
+        })
+        .catch(err => {
+            console.log('get post by id error', err);
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
 router.get('/:id/comments', (req, res) => { 
     const id = req.params.id;
     Data.findById(id)
